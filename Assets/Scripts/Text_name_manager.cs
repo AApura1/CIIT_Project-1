81776c80ee11807327d8532f5735d2b128a0b7f9
AApura1 <134472388+AApura1@users.noreply.github.com>
2023-05-31 14:54:33 +0800

    Added text game

---
+++ Assets/Scripts/Text_name_manager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Text_name_manager : MonoBehaviour
+{
+    public int HP_value, Sanity_value;
+    public TextMeshProUGUI HPtextValue, SanitytextValue, storytextValue;
+    public string startofstory;
+    public Gameobject level1Choices;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP_value = 10;
+        Sanity_value = 10;
+        startofstory "I woke up then back to sleep";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HPtextValue.text = HP_value.ToString();
+        SanitytextValue.text = Sanity_value.ToString();
+        storytextValue.text = startofstory;
+
+    }
+
+    public void Goupstairs()
+    {
+        HP_value -= 10;
+        startofstory = "You went back to sleep but you went into a coma";
+        level1Choices.SetActive(false);
+    }
+    public void Sleep()
+    {
+        HP_value -= 10;
+        startofstory = "You went back to sleep but you went into a coma";
+    }
+    public void Brushteeth()
+    {
+        HP_value -= 10;
+        startofstory = "You brush to well, you started to bleed";
+    }
+}
